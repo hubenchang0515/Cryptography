66015dab6a7093a1cb3751e545ad62d8ba24ec86
hubenchang0515 <hubenchang0515@outlook.com>
2019-12-12 22:05:41 +0800

    new demo for new interface

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ crypto : main.o md5.o sha1.o sha2.o
 	gcc -o crypto main.o md5.o sha1.o sha2.o -W -Wall -O3 
 
 main.o : main.c md5.h sha1.h sha2.h
-	gcc -c main.c -W -Wall -O3 
+	gcc -c main.c -W -Wall -O3 -Wno-incompatible-pointer-types
 
 md5.o : md5.c md5.h
 	gcc -c md5.c -W -Wall -O3 
--- main.c
+++ main.c
@@ -5,38 +5,32 @@
 #include "sha1.h"
 #include "sha2.h"
 
-/* callback */
-int getData(void* fp, size_t length, void* data)
-{
-	return fread(data,1,length,(FILE*)fp);
-}
-
-
-typedef struct CallListNode{
-	const char* key;
-	void (*func)(Md5Callback , void*, char*);
-}CallListNode;
-
-
-/* Options List */
-CallListNode callList[] = 
-{
-	{"md5", md5Universal},
-	{"sha1", sha1Universal},
-	{"sha224", sha224Universal},
-	{"sha256", sha256Universal},
-	{"sha384", sha384Universal},
-	{"sha512", sha512Universal},
-	{NULL,NULL}
-};
-
-
 const char* filename(const char* filepath)
 {
 	const char* p = filepath + strlen(filepath) - 1;
 	for(; p >= filepath && *p != '/' && *p != '\\'; p--);
 	return p+1;
 }
+
+typedef struct CallListNode{
+	const char* key;
+	void (*initFunc)(void* state);
+	void (*updateFunc)(void* state, const void* data, size_t length);
+	const char* (*hexFunc)(void* state);
+}CallListNode;
+
+
+/* Options List [gcc -Wno-incompatible-pointer-types] */
+CallListNode callList[] = 
+{
+	{"md5", md5Reset, md5Update, md5Hex},
+	{"sha1", sha1Reset, sha1Update, sha1Hex},
+	{"sha224", sha224Reset, sha224Update, sha224Hex},
+	{"sha256", sha256Reset, sha256Update, sha256Hex},
+	{"sha384", sha384Reset, sha384Update, sha384Hex},
+	{"sha512", sha512Reset, sha512Update, sha512Hex},
+	{NULL, NULL, NULL, NULL}
+};
 
 int main(int argc, char* argv[])
 {	
@@ -48,16 +42,24 @@ int main(int argc, char* argv[])
 		{
 			if(!strcmp(argv[1],node->key) )
 			{
-				FILE* fp = fopen(argv[2],"rb");
+				FILE* fp = fopen(argv[2], "rb");
 				if(fp == NULL)
 				{
 					printf("%s\n",strerror(errno));
 					return 1;
 				}
-				char output[256];
-				node->func(getData, fp, output);
+				char state[sizeof(Sha512)];
+				char buffer[1024];
+				size_t len;
+				node->initFunc(state);
+
+				while((len = fread(buffer, 1, 1024, fp)) > 0)
+				{
+					node->updateFunc(state, buffer, len);
+				}
+				
 				fclose(fp);
-				printf("%s\n", output);
+				printf("%s\n", node->hexFunc(state));
 				return 0;
 			}
 		}
